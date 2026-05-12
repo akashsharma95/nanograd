fdd39a5ae2c51094966ea3db8a1647c276ebd958
akashsharma95 <akashsharma@live.in>
2026-05-12 16:18:10 +0100

    Add add and mul operation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::{cell::RefCell, rc::Rc};
+use std::ops::{Add, Mul};
 
 #[derive(Clone)]
 pub struct Value {
@@ -65,6 +66,25 @@ impl Value {
     }
 }
 
+impl Add for Value {
+    type Output = Value;
+
+    fn add(self, rhs: Value) -> Self::Output {
+        let data = self.data() + rhs.data();
+
+        Value::from_op(data, vec![self, rhs], Op::Add)
+    }
+}
+
+impl Mul for Value {
+    type Output = Value;
+
+    fn mul(self, rhs: Value) -> Self::Output {
+        let data = self.data() * rhs.data();
+        Value::from_op(data, vec![self, rhs], Op::Mul)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -74,5 +94,21 @@ mod tests {
         let a = Value::new(1.0);
         let b = a.relu();
         assert_eq!(b.data(), 1.0);
+    }
+
+    #[test]
+    fn test_add() {
+        let a = Value::new(1.0);
+        let b = Value::new(2.0);
+        let c = a + b;
+        assert_eq!(c.data(), 3.0);
+    }
+    
+    #[test]
+    fn test_mul() {
+        let a = Value::new(1.0);
+        let b = Value::new(2.0);
+        let c = a * b;
+        assert_eq!(c.data(), 2.0);
     }
 }
