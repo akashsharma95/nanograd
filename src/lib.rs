6d8466a082260f1143375fe9696c0935a84aeefe
akashsharma95 <akashsharma@live.in>
2026-05-12 16:14:35 +0100

    Add simple activation function relu

--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,60 @@ enum Op {
     Mul,
     Relu,
 }
+
+impl Value {
+    pub fn new(data: f64) -> Self {
+        Self {
+            inner: Rc::new(RefCell::new(Node {
+                data,
+                grad: 0.0,
+                prev: Vec::new(),
+                op: Op::Leaf,
+            })),
+        }
+    }
+
+    fn from_op(data: f64, prev: Vec<Value>, op: Op) -> Self {
+        Self {
+            inner: Rc::new(RefCell::new(Node {
+                data,
+                grad: 0.0,
+                prev,
+                op,
+            })),
+        }
+    }
+
+    pub fn data(&self) -> f64 {
+        self.inner.borrow().data
+    }
+
+    pub fn grad(&self) -> f64 {
+        self.inner.borrow().grad
+    }
+
+    pub fn set_grad(&self, grad: f64) {
+        self.inner.borrow_mut().grad = grad;
+    }
+
+    pub fn add_grad(&self, grad: f64) {
+        self.inner.borrow_mut().grad += grad;
+    }
+
+    pub fn relu(&self) -> Self {
+        let data = if self.data() > 0.0 { self.data() } else { 0.0 };
+        Self::from_op(data, vec![self.clone()], Op::Relu)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_relu() {
+        let a = Value::new(1.0);
+        let b = a.relu();
+        assert_eq!(b.data(), 1.0);
+    }
+}
